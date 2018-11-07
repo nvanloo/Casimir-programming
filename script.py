1729d102fe9633fb8f51e1cc6ba29ec72de2d721
nvanloo <n.nickvanloo@gmail.com>
2018-11-07 13:46:06 +0100

    Added functionality to actually calculate circumference and area.

--- script.py
+++ script.py
@@ -2,3 +2,6 @@ print('Just another print statement. Nothing special.')
 
 import test
 
+var = input('What is the radius of the circle?')
+
+print('Circumference is', test.Circumference(float(var)), 'and area is', test.Surface_area(float(var)))
